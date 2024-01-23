43a01665923c78daa6e3be06eaef3a2e0ee4e36a
Dhaval <ninjadhaval@gmail.com>
2024-01-23 17:59:23 +0100

    distill_config

--- mmpose/configs/body_2d_keypoint/multiteacher/coco/distill_config.py
+++ mmpose/configs/body_2d_keypoint/multiteacher/coco/distill_config.py
@@ -19,18 +19,16 @@ model = dict(
             dict(
                 type='KDLoss',
                 name='loss_logit_coco',
-                use_this=logit,
-                weight = 0.3
+                use_this=logit
             ),
             dict(
                 type='KDLoss',
                 name='loss_logit_mpii',
-                use_this=logit,
-                weight = 0.3
+                use_this=logit
             )
         ]),
     ],
-    weight=[1.0, 0.35, 0.35], # [student_weight, coco_teacher_weight, mpii_teacher_weight]
+    weight=[0.3, 0.35, 0.35], # [student_weight, coco_teacher_weight, mpii_teacher_weight]
     teacher1_pretrained='work_dirs/rtmpose-m_8xb256-420e_coco-256x192/best_coco_AP_epoch_100.pth',
     teacher2_pretrained='work_dirs/rtmpose-m_8xb64-210e_mpii-256x256/best_PCK_epoch_100.pth',
     # train_cfg=train_cfg,
